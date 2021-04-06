f0db1c126d7acb5f52af47e2a19fc9dd746f2fc9
Maomaoyu0319 <70495718+Maomaoyu0319@users.noreply.github.com>
2021-04-06 23:18:09 +0800

    Update jd_beauty.js

--- Node/jd_beauty.js
+++ Node/jd_beauty.js
@@ -92,7 +92,7 @@ async function mr() {
   const WebSocket = require('ws')
   let client = new WebSocket(`wss://xinruimz-isv.isvjcloud.com/wss/?token=${$.token}`,null,{
     headers:{
-      'user-agent': process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1",
+      'user-agent': process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : "jdapp;iPhone;9.4.6;14.3;b82f74cfa8b940f2cee7ec3546f7b7f0334c7882;network/wifi;ADID/15EF823D-119E-470D-AAFC-D8656322BA15;supportApplePay/0;hasUPPay/0;hasOCPay/0;model/iPhone12,1;addressid/580567206;supportBestPay/0;appBuild/167618;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
     }
   })
   console.log(`wss://xinruimz-isv.isvjcloud.com/wss/?token=${$.token}`)
